6c19a7efbdb7459579094822f98b8c04da8f004f
hodhod222 <aliezadkhaha212@gmail.com>
2022-11-21 07:44:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+const app = express();
+const dotenv = require("dotenv");
+const env = dotenv.config();
+app.enable("trust proxy");
+
+const PORT = process.env.PORT || 9001;
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
+
+const peerServer = ExpressPeerServer(server, {
+  path: "/",
+});
+
+app.use("/myapp", peerServer);
+
+module.exports = app;
