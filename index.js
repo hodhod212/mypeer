c703776c840fdcb587d26d26869fee5b2fb0c4da
hodhod222 <aliezadkhaha212@gmail.com>
2022-11-21 08:17:15 +0100

    created peer

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-var peer_1 = require("peer");
-(0, peer_1.PeerServer)({ port: 9001, path: "/" });
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+const app = express();
+
+app.enable("trust proxy");
+
+//const PORT = process.env.PORT;
+const PORT = 9001;
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
+
+const peerServer = ExpressPeerServer(server, {
+  path: "/",
+});
+
+app.use("/myapp", peerServer);
 
 module.exports = app;
